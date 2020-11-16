7742e734252dbe3158f7359ab92565228a4ce25b
Shelbie Fecher <63127198+Sfecher@users.noreply.github.com>
2020-11-16 16:54:33 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php session_start();
 include 'functions.php'; ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="style.css" rel="stylesheet" type="text/css"/>
@@ -13,7 +13,7 @@ include 'functions.php'; ?>
 <body>
   <div class="topnav">
     <div class="topnav-img" id="logo">
-        <img src="logo.png" width="45" height="45" draggable="false">
+        <img src="logo.png" width="45" height="45" draggable="false" alt="logo">
       </div>
     <a href="/" id="home" draggable="false">Talko Forum</a>
     <?php loginBar(); ?>
