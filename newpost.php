e4e76eb4dd49a9931b6eef9bf25e8d20f8a3a74e
Shelbie Fecher <63127198+Sfecher@users.noreply.github.com>
2020-11-16 16:40:40 -0500

    Update newpost.php

--- newpost.php
+++ newpost.php
@@ -4,7 +4,7 @@ include 'functions.php';
 $mysqli = connect();
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="style.css" rel="stylesheet" type="text/css"/>
