5864b618611beebd9182288397c82719de72b247
Shelbie Fecher <63127198+Sfecher@users.noreply.github.com>
2020-11-16 09:13:37 -0500

    Update introductions.php

--- introductions.php
+++ introductions.php
@@ -1,3 +1,8 @@
+<?php session_start();
+include 'db-config.inc.php';
+include 'functions.php';
+$mysqli = connect();
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -14,7 +19,32 @@
         <img src="logo.png" width="45" height="45" draggable="false">
       </div>
     <a href="/" id="home" draggable="false">Talko Forum</a>
-    <a href="/register.php" id="register" draggable="false">Register</a>
-    <a href="/login.php" id="login" draggable="false">Login</a>
+    <?php loginBar(); ?>
   </div>
+  <form method = "post" action = "newpost.php">
+    <input type="submit" class="btn btn-primary btn-lg active btn-rounded login" value="New Post" style="display:inline-block;  font-family: 'Lucida Console', Courier, monospace; float:left; margin-top: 25px; background-color:#2F1B1B; color:white">
+  </form>
+    <h3 id="divider" style="margin-top: 45px; position: sticky; display:block">Posts</h3>
+  <table border="1" style="border:8px;">
+
+      <tr><th>Title:</th><th>User:</th><th>Date:</th>
+    <?php
+    $sql = "SELECT * FROM `posts` WHERE 1 ORDER BY `post_date` DESC";
+    $result = $mysqli->query($sql);
+    while ($row = $result->fetch_assoc()) {
+      $topic = $row['post_topic'];
+      $postID = $row['post_id'];
+      $date = $row['post_date'];
+      $postBy = $row['post_by'];
+      echo
+      '<tr>'.
+        '<td><a href="topic.php?id='.$postID.'">'.$topic.'</a></td>'.
+        '<td>'. $postBy .'</td>'.
+        '<td style="text-align: center">'. $date .'</td>'.
+      '</tr>';
+    }
+     ?>
+   </table>
+  <footer id="footer" style="position: absolute; text-align: center; bottom: 0; width: 100%; height: 2.5rem;">
+  <?php footerBar();  ?>
 </html>
