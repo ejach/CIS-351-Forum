08b9e0eff69ebf7d91da9218b71a69f558445e07
Shelbie Fecher <63127198+Sfecher@users.noreply.github.com>
2020-11-16 09:08:00 -0500

    Update account.php

--- account.php
+++ account.php
@@ -1,19 +1,19 @@
 <?php
-session_start();
-if ($_SESSION['signed_in']) {
-  //. $_SESSION['username']
-  $imagePath = '/var/www/html/imgs/' . $_SESSION["username"]);
-  if (!file_exists($imagePath) {
-      mkdir($imagePath , 0777, true);
+  session_start();
+  if ($_SESSION['signed_in']) {
+    //. $_SESSION['username']
+    $imagePath = '/var/www/html/imgs/' . $_SESSION["username"];
+    if (!is_dir($imagePath)) {
+        mkdir($imagePath , 0777, true);
+    }
+  } else {
+    echo '<p id="welcome">Welcome unknown user! Please <a href="/login.php">sign in</a> or <a href="/register.php">create an account.</a></p><br>';
   }
-} else {
-  echo '<p id="welcome">Welcome unknown user! Please <a href="/login.php">sign in</a> or <a href="/register.php">create an account.</a></p><br>';
-}
-    include 'accountFunctions.php';
-    $mysqli = new mysqli(DBHOST, DBUSER, DBPASSWORD, DB);
-    $sql = "SELECT * FROM profile";
-    $res = $mysqli->query($sql);
-    $prof = $res->fetch_assoc();
+      include 'accountFunctions.php';
+      $mysqli = new mysqli(DBHOST, DBUSER, DBPASSWORD, DB);
+      $sql = "SELECT * FROM profile";
+      $res = $mysqli->query($sql);
+      $prof = $res->fetch_assoc();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -24,9 +24,20 @@ if ($_SESSION['signed_in']) {
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
         <title>ForumTalko</title>
     </head>
-    <body class="loginBody">
+    <div class="topnav">
+      <div class="topnav-img" id="logo">
+          <img src="logo.png" width="45" height="45" draggable="false" alt="logo">
+        </div>
+      <a href="/" id="home" draggable="false">Talko Forum</a>
+      <?php loginBar(); ?>
+    <body style="background:white" class="loginBody">
         <div class = "main">
         <div class = "wrapper">
+          <?php
+            $destination = "http://talkoforum.ngrok.io/imgs/" . $_SESSION["username"] . "/" . $_SESSION["username"] . '.jpg';
+            echo "<img src='$destination' width='300' height='300'/>";
+           ?>
+
           <form enctype="multipart/form-data" method="POST" action="uploadImage.php">
             <input type="file" name="file1"><br>
             <input style="background-color:#2F1B1B;" class="btn btn-primary btn-md active btn-rounded buttonStyle" type="submit" value="SUBMIT IMAGE">
@@ -43,8 +54,8 @@ if ($_SESSION['signed_in']) {
                 <input class="input" type="password" name="vpass" id="vpass" placeholder=" Retype Password...">
                 <br>
                 <?php
-                    updateEmail($_POST["email"], $_SESSION["user"]);
-                    updatePassword($_POST["npass"], $_POST["vpass"], $_SESSION["user"]);
+                    updateEmail($_POST["email"], $_SESSION["username"]);
+                    updatePassword($_POST["npass"], $_POST["vpass"], $_SESSION["username"]);
                 ?>
                 <br>
                 <input type="submit" style="background-color:#2F1B1B;" class="btn btn-primary btn-lg active btn-rounded buttonStyle" value="SAVE CHANGES">
